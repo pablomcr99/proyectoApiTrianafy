079fc946ac121eb423ac1611b65c7862223f30fe
pablomcr99 <pablomancina@gmail.com>
2021-01-27 13:05:23 +0100

    rutas y controladores acabados

--- trianafy/src/controllers/listsController.js
+++ trianafy/src/controllers/listsController.js
@@ -4,3 +4,69 @@
 // put /list/{id}
 
 //delete /list/{id}
+
+// post /lists/{id1}/song/{id2}
+// get /lists/{id1}/song/{id2}
+// get /lists/{id}/songs
+// delete /lists/{id1}/song/{id2}
+
+import {ListaRepository} from '../models/lista';
+
+
+const ListaController = {
+
+
+
+//get /lists/
+    todasLaslistas: async (req, res) => {
+        const data = await ListaRepository.findAll();
+        if (Array.isArray(data) && data.length > 0){
+            res.json(data);
+        }
+        else
+            res.sendStatus(404);
+    },
+
+// get list/{id}
+    listaPorId: async (req, res) => {
+        let cancion = await ListaRepository.findById(req.params.id);
+        if (cancion != undefined)
+            res.json(cancion);
+        else
+            res.sendStatus(404);
+    },
+
+//post list
+    nuevaLista: async (req, res) => {
+        let lista = await ListaRepository.create({
+            
+        });
+        res.status(201).json(lista);
+    },
+
+    editarLista: async (req, res) => {
+        let lista = await ListaRepository
+            .updateById(req.params.id, {
+                
+            });
+        if (lista != undefined) {
+            res.json(lista);
+        } else {
+            res.sendStatus(404);
+        }
+    },
+
+    eliminarLista: async (req, res) => {
+        await ListaRepository.delete(req.params.id);
+        res.sendStatus(204);
+    },
+
+    
+
+
+
+}
+
+export {
+    ListaController
+}
--- trianafy/src/routes/lists.js
+++ trianafy/src/routes/lists.js
@@ -4,3 +4,24 @@
 // put /list/{id}
 
 //delete /list/{id}
+
+
+import { Router } from 'express';
+import { ListaController } from '../controllers/listsController';
+import {token} from '../services/passport';
+
+const router = Router();
+
+router.get('/lists',token(), ListaController.todasLaslistas);
+
+router.get('/list/:id',token(), ListaController.listaPorId);
+
+router.post('/list/',token(), ListaController.nuevaLista);
+
+router.put('/list/:id',token(), ListaController.editarLista);
+
+router.delete('/list/:id',token(), ListaController.eliminarLista);
+
+
+
+export default router;
